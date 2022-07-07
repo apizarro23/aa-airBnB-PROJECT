3d43a42734a1ac3b2a05133df7cad81b11f6fd47
Antony Pizarro <apizarro23@live.com>
2022-07-07 18:32:53 -0400

    files added

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,23 +2,12 @@
 const config = require('./index');
 
 module.exports = {
-  // development: {
-  //   storage: config.dbFile,
-  //   dialect: "sqlite",
-  //   seederStorage: "sequelize",
-  //   logQueryParameters: true,
-  //   typeValidation: true
-  // },
   development: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
   },
   production: {
     use_env_variable: 'DATABASE_URL',
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {requireAuth } = require('../../utils/auth');
-const {Spot, Image, User, Review} = require('../../db/models');
+const {Spot, Image, User, Review, sequelize} = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
-const {Sequelize} = require('sequelize');
-const sequelize = new Sequelize("sqlite::memory:");
+// const {Sequelize} = require('sequelize');
+// const sequelize = new Sequelize("sqlite::memory:");
 
 
 const validateSpot = [
