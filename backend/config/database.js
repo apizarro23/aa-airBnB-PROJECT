db70ed3d2a2d2119d40d65c0c2547fe0fe0cfcab
Antony Pizarro <apizarro23@live.com>
2022-07-07 18:23:00 -0400

    files added

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,14 +2,14 @@
 const config = require('./index');
 
 module.exports = {
+  // development: {
+  //   storage: config.dbFile,
+  //   dialect: "sqlite",
+  //   seederStorage: "sequelize",
+  //   logQueryParameters: true,
+  //   typeValidation: true
+  // },
   development: {
-    storage: config.dbFile,
-    dialect: "sqlite",
-    seederStorage: "sequelize",
-    logQueryParameters: true,
-    typeValidation: true
-  },
-  production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
