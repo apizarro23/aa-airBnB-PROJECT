b75d663055ff6f1b396f9c3159a61600e3dd5e12
Antony Pizarro <apizarro23@live.com>
2022-08-01 08:27:26 -0400

    all in

--- frontend/src/components/SignUpFormPage/index.js
+++ frontend/src/components/SignUpFormPage/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
-import "./SignupForm.css";
+import "./SignUpForm.css";
 
 function SignupFormPage() {
   const dispatch = useDispatch();
