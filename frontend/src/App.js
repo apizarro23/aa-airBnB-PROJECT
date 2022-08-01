266101adae8258ab08ca245c84343d55496b77f4
Antony Pizarro <apizarro23@live.com>
2022-08-01 07:48:24 -0400

    all in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-// import SignupFormPage from "./components/SignupFormPage";
-import SignupFormPage from "./components/SignupFormPage/index"
+import SignupFormPage from "./components/SignupFormPage";
 import LoginFormModal from "./components/LoginFormModal";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
